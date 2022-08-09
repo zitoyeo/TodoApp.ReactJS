de8d6d2216e7015dd42c8d0cf109dac80bf16a84
zitoyeo <wlsgh010203@gmail.com>
2022-08-09 21:34:07 +0900

    add reset button

--- src/app.jsx
+++ src/app.jsx
@@ -51,6 +51,14 @@ class App extends Component {
     this.setState({ habits });
   };
 
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  };
+
   render() {
     return (
       <>
@@ -63,6 +71,7 @@ class App extends Component {
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
           onAdd={this.handleAdd}
+          onReset={this.handleReset}
         />
       </>
     );
