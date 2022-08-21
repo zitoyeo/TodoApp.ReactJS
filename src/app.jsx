e3984b35ad147dd16b79f6c652cbd4ad5866bbf0
zitoyeo <wlsgh010203@gmail.com>
2022-08-21 14:03:37 +0900

    use PureComponent

--- src/app.jsx
+++ src/app.jsx
@@ -25,19 +25,32 @@ class App extends Component {
   };
 
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits]; //기본으로 가지고 있는 state.habits을 ...(스프레드 오퍼레이터)로 복사해옴
-    const index = habits.indexOf(habit); //habit에 몇번째 있는지 찾을 수 있는 indexOf()Api사용
-    habits[index].count++;
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
+    // [...this.state.habits]; //기본으로 가지고 있는 state.habits을 ...(스프레드 오퍼레이터)로 복사해옴
+    // const index = habits.indexOf(habit); //habit에 몇번째 있는지 찾을 수 있는 indexOf()Api사용
+    // habits[index].count++;
     this.setState({ habits });
   };
 
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count; //직접 수정했으니 썩 좋은건 아님.
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
+    // [...this.state.habits];
+    // const index = habits.indexOf(habit);
+    // const count = habits[index].count - 1;
+    // habits[index].count = count < 0 ? 0 : count; //직접 수정했으니 썩 좋은건 아님.
 
-    this.setState(this.state);
+    this.setState({ habits });
   };
 
   handleDelete = (habit) => {
@@ -53,7 +66,9 @@ class App extends Component {
 
   handleReset = () => {
     const habits = this.state.habits.map((habit) => {
-      habit.count = 0;
+      if (habit.count !== 0) {
+        return { ...habit, count: 0 };
+      }
       return habit;
     });
     this.setState({ habits });
--- src/components/habitaddForm.jsx
+++ src/components/habitaddForm.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class HabitaddForm extends Component {
+class HabitaddForm extends PureComponent {
   inputRef = React.createRef();
   formRef = React.createRef();
   onSubmit = (event) => {
