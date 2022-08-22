87cb8ca6dd0e39b3d24aa6d73af7492c788eb3f4
zitoyeo <wlsgh010203@gmail.com>
2022-08-22 23:08:29 +0900

    use memo & change component => hooks

--- src/components/habitaddForm.jsx
+++ src/components/habitaddForm.jsx
@@ -1,29 +1,24 @@
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 
-class HabitaddForm extends PureComponent {
-  inputRef = React.createRef();
-  formRef = React.createRef();
-  onSubmit = (event) => {
+const HabitaddForm = memo((props) => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
+  const onSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name);
-    this.formRef.current.reset();
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+    formRef.current.reset();
   };
-  render() {
-    return (
-      <div>
-        <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-          <input
-            ref={this.inputRef}
-            type="text"
-            className="add-input"
-            placeholder="입력해주세요"
-          />
-          <button className="add-button">Add</button>
-        </form>
-      </div>
-    );
-  }
-}
-
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="입력해주세요"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
 export default HabitaddForm;
